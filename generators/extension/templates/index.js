60d63caf0a51031d3248f799a0724310676c2613
Kristy Miller <miller.kristy06@gmail.com>
2016-09-07 11:19:12 -0500

    Update extension index template to look for generators as well as commands

--- generators/extension/templates/index.js
+++ generators/extension/templates/index.js
@@ -1,19 +1,32 @@
-const fs = require('fs');
 const resolve = require('resolve');
+const glob = require('glob');
+const path = require('path');
+const packageJson = require('./package.json');
 
-const findExtensionCommands = register => {
-  fs.readdirSync(`${__dirname}/commands`).map(file => {
-    try {
-      const commandPath = resolve.sync(file, { moduleDirectory: 'commands' });
-      const command = require(commandPath);
+const findExtensionCommands = registerFunction => {
+  const commands = glob.sync('commands/**.js', { cwd: __dirname });
 
-      register(command);
-    } catch (error) {
-      console.warn(error);
-    }
+  commands.forEach(commandFileName => {
+    const commandPath = resolve.sync(`./${commandFileName}`);
+    const command = require(commandPath);
+
+    registerFunction(command);
+  });
+};
+
+const findExtensionGenerators = registerFunction => {
+  const generators = glob.sync('generators/*/index.js', { cwd: __dirname });
+
+  generators.forEach(generator => {
+    const generatorPath = resolve.sync(`./${generator}`);
+    const directory = path.dirname(generatorPath).split('/').pop();
+    const namespace = `${packageJson.name}:${directory}`;
+
+    registerFunction(generatorPath, namespace);
   });
 };
 
 module.exports.register = stanza => {
   findExtensionCommands(stanza.registerCommand);
+  findExtensionGenerators(stanza.registerGenerator);
 };
