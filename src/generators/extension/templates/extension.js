8d4413bcd470a722602fd05709fc35dd457afb0a
Kristy Miller <miller.kristy06@gmail.com>
2016-09-21 10:06:04 -0500

    Update extension template to new structure

--- src/generators/extension/templates/extension.js
+++ src/generators/extension/templates/extension.js
@@ -1,13 +1,14 @@
-import yeoman from 'yeoman-environment';
 import Extension from '../../stanza-cli/src/extension';
 
 /**
  * <%= extensionName %> representing an Extension
 * */
-class <%= extensionName %> extends Extension {
+export default class <%= extensionName %> extends Extension {
   constructor(name, registerWithObject, keyword) {
     super(name, registerWithObject, keyword, __dirname);
+
+    this._extensionPath = __dirname;
+
+    this.discoverCommands(`${this._extensionPath}/commands`);
   }
 }
-
-export default <%= extensionName %>;
