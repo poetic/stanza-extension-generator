7a307cd48a9bdbe32b3b7967a4d092992894cda8
Kristy Miller <miller.kristy06@gmail.com>
2016-09-30 16:17:13 -0500

    Create generator command when generatoring a generatorC

--- src/generators/generator/index.js
+++ src/generators/generator/index.js
@@ -5,6 +5,10 @@ const yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
   initializing() {
+    this.generatorClassName = this.options.name
+    .replace(/(\w)(\w*)/g, (g0, g1, g2) => g1.toUpperCase() + g2.toLowerCase())
+    .replace(/-/g, '');
+
     this.destinationRoot(`${this.destinationRoot()}/src/generators/${this.options.name}`);
   },
 
@@ -20,6 +24,17 @@ module.exports = yeoman.Base.extend({
         name: 'name',
         message: 'Name?',
         default: this.options.name,
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What will we be generatoring?',
+      },
+      {
+        type: 'input',
+        name: 'command',
+        message: 'What command should be us to call this generator?',
+        default: `generate-${this.options.name}`,
       },
     ];
 
@@ -66,12 +81,23 @@ module.exports = yeoman.Base.extend({
   },
 
   writing() {
-    this.fs.copyTpl(
+    this.fs.copy(
       this.templatePath('index.js'),
       this.destinationPath('index.js')
     );
 
     this.fs.copyTpl(
+      this.templatePath('command.js'),
+      this.destinationPath(`../${this.props.name}.js`),
+      {
+        generatorClassName: this.generatorClassName,
+        name: this.props.name,
+        description: this.props.description,
+        command: this.props.command,
+      }
+    );
+
+    this.fs.copy(
       this.templatePath('example.js'),
       this.destinationPath('templates/example.js')
     );
